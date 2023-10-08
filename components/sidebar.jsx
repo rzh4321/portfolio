b482b7c7f0cd5a4df65cdd181929018239f6fea6
Ricky <rzh4321@gmail.com>
2023-10-07 22:08:35 -0400

    refactored sidebar component

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -20,6 +20,19 @@ const navLinks = {
 };
 
 export default function SideBar() {
+  return (
+    <aside>
+      <nav className="flex flex-col flex-wrap pr-2 md:sticky md:top-20">
+        <Link className="pl-2 text-3xl md:w-auto font-medium" href="/">
+          Ricky
+        </Link>
+        <SideBarList />
+      </nav>
+    </aside>
+  );
+}
+
+function SideBarList() {
   const [activeNavLink, setActiveNavLink] = useState("/");
   const pathname = usePathname();
 
@@ -29,35 +42,32 @@ export default function SideBar() {
   });
 
   return (
-    <aside>
-      <nav className="flex flex-col flex-wrap pr-2 md:sticky md:top-20">
-        <Link className="pl-2 text-3xl md:w-auto font-medium" href="/">
-          Ricky
-        </Link>
-        <ul className="flex flex-row md:flex-col">
-          {Object.keys(navLinks).map((link) => (
-            <li
-              key={link}
-              className={`rounded px-2 py-1 hover:text-black 
-              ${
-                activeNavLink === link ? "bg-gray-100 dark:bg-neutral-800" : ""
-              }`}
-            >
-              <Link
-                href={link}
-                className={`font-medium hover:text-black dark:hover:text-gray-200 
-                ${
-                  activeNavLink === link
-                    ? "dark:text-gray-200 text-black"
-                    : "text-neutral-500"
-                }`}
-              >
-                {navLinks[link].name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </aside>
+    <ul className="flex flex-row md:flex-col">
+      {Object.keys(navLinks).map((link) => (
+        <SideBarLink key={link} link={link} activeNavLink={activeNavLink} />
+      ))}
+    </ul>
+  );
+}
+
+function SideBarLink({ link, activeNavLink }) {
+  return (
+    <li
+      key={link}
+      className={`rounded px-2 py-1 hover:text-black 
+        ${activeNavLink === link ? "bg-gray-100 dark:bg-neutral-800" : ""}`}
+    >
+      <Link
+        href={link}
+        className={`font-medium hover:text-black dark:hover:text-gray-200 focus-brand
+          ${
+            activeNavLink === link
+              ? "dark:text-gray-200 text-black"
+              : "dark:text-neutral-400 text-neutral-500"
+          }`}
+      >
+        {navLinks[link].name}
+      </Link>
+    </li>
   );
 }
